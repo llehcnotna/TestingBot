002f087cd32e05909f41fb16980556351ef2a9af
llehcnotna <46939025+llehcnotna@users.noreply.github.com>
2020-03-10 10:46:36 -0600

    Create app.js

---
+++ app.js
@@ -0,0 +1,70 @@
+const config = require('./config.json'); // requires prefix and bot token
+const Discord = require('discord.js'); // requires discord.js
+const ddiff = require('return-deep-diff'); // requires ddiff
+const moment = require('moment');
+
+
+const fs = require('fs'); // require files
+
+const client = new Discord.Client({disableEveryone: true});
+client.commands = new Discord.Collection();
+
+//command handler setup
+fs.readdir('./commands', (err, files) => {
+
+    if(err) console.log(err);
+
+    let jsfile = files.filter(f => f.split('.').pop() === 'js');
+    if (jsfile.length <= 0){
+        console.log('couldn\'t find commands.');
+        return;
+    }
+
+    jsfile.forEach((f, i) => {
+        let props = require(`./commands/${f}`);
+        console.log(`[ LOG ] ${f} loaded!`);
+        client.commands.set(props.help.name, props);
+    });
+
+
+});
+//client events (playing, watching, streaming...)
+client.on("ready", async () => {
+    console.log(`\n[ LOG ] ${client.user.username} is online!`); // checking if the bot is online.
+    client.user.setStatus('online'); // Change Status
+    function setActivity() {
+        //Variable Array for what the setGame can be set to
+        var Gameinfo = [`${config.prefix}help`, `Javascript` // Change these to what you want, add as many or as few as you want to
+        ]
+    
+        var info = Gameinfo[Math.floor(Math.random() * Gameinfo.length)]; //Random Math to set the setGame to something in the GameInfo array
+    
+        client.user.setActivity(info) // "playing Game" '...' Sets the setGame to what the info Random math picked from the GameInfo Array
+        if (config.debugMode === "1") {
+            console.log(`[ LOG ] set Activity set to ( ${info} )`) //Logs to console what the setGame was set as.
+        }
+    
+    }
+    
+    setInterval(setActivity, 1000 * 60 * 10) //sets and picks a new game every 10 minutes
+    });
+
+//perms stuffs..
+client.on('message', async message => {
+
+    if (message.author.bot) return; // If message author = bot return
+    if (message.channel.type === "dm") return; // If message sent in DM's return
+
+    let prefix = config.prefix; // let prefix = config.prefix
+    if(!message.content.startsWith(prefix)) return; // If message does not start with Prefix return
+    let messageArray = message.content.split(" ");
+    let command = messageArray[0];
+    let args = messageArray.slice(1)
+
+    let commandfile = client.commands.get(command.slice(prefix.length));
+    if (commandfile) commandfile.run(client, message, args);
+
+    // const command = args.shift().toLowerCase();
+ });
+
+client.login(process.env.BOT_TOKEN); // login token
