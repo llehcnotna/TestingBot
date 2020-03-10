bad06a0cc5af5103f060cea7909731b4310da208
llehcnotna <46939025+llehcnotna@users.noreply.github.com>
2020-03-10 10:57:20 -0600

    Update app.js

--- app.js
+++ app.js
@@ -3,6 +3,33 @@ const Discord = require('discord.js'); // requires discord.js
 const ddiff = require('return-deep-diff'); // requires ddiff
 const moment = require('moment');
 
+// TEST //
+var http = require('http'); //importing http
+
+function startKeepAlive() {
+    setInterval(function() {
+        var options = {
+            host: 'your_app_name.herokuapp.com',
+            port: 80,
+            path: '/'
+        };
+        http.get(options, function(res) {
+            res.on('data', function(chunk) {
+                try {
+                    // optional logging... disable after it's working
+                    console.log("HEROKU RESPONSE: " + chunk);
+                } catch (err) {
+                    console.log(err.message);
+                }
+            });
+        }).on('error', function(err) {
+            console.log("Error: " + err.message);
+        });
+    }, 20 * 60 * 1000); // load every 20 minutes
+}
+
+startKeepAlive();
+//////////
 
 const fs = require('fs'); // require files
 
